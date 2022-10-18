a5d7efb6f3dfe128a1d12112fd72c14c4d73fbfa
Visto-Preto <evppdepf@protonmail.ch>
2022-10-18 17:58:57 -0300

    first commit

---
+++ App.py
@@ -0,0 +1,14 @@
+#coding: UTF-8
+from flask import Flask, render_template, url_for
+
+__author__ = "VistoPreto"
+
+app = Flask(__name__)
+
+@app.route('/')
+def	homepage():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	app.run(debug=True, host="0.0.0.0")
+
---
+++ thumbnail.py
@@ -0,0 +1,2 @@
+import os
+os.system(r"ffmpeg -i static\videos\input.mp4 cache\input.png")
